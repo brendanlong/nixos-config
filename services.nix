777e31c4da3b676e83cad06256180d596ae8120f
Brendan Long <self@brendanlong.com>
2015-01-06 05:25:25 +0100

    Be less repetitive in the services.nix file.

--- services.nix
+++ services.nix
@@ -1,24 +1,26 @@
 {
-  services.httpd = {
-    enable = true;
-    adminAddr = "self@brendanlong.com";
-    # Note, run the following:
-    #   chmod 711 ~blong
-    #   chown blong:wwwrun -R ~blong/public_html
-    #   chmod g+s ~blong/public_html
-    # Would be nice to figure out how to make nix do that.
-    documentRoot = "/home/blong/public_html";
-  };
+  services = {
+    httpd = {
+      enable = true;
+      adminAddr = "self@brendanlong.com";
+      # Note, run the following:
+      #   chmod 711 ~blong
+      #   chown blong:wwwrun -R ~blong/public_html
+      #   chmod g+s ~blong/public_html
+      # Would be nice to figure out how to make nix do that.
+      documentRoot = "/home/blong/public_html";
+    };
 
-  services.openssh = {
-    enable = true;
-    permitRootLogin = "no";
-    passwordAuthentication = false;
-  };
+    openssh = {
+      enable = true;
+      permitRootLogin = "no";
+      passwordAuthentication = false;
+    };
 
-  services.syncthing = {
-    enable = true;
-    user = "blong";
-    dataDir = "/home/blong";
+    syncthing = {
+      enable = true;
+      user = "blong";
+      dataDir = "/home/blong";
+    };
   };
 }
